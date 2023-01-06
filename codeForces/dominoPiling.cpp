1759adfc386ab51dba5bd06eff545150e26ca38d
Fatema Moharam <89610248+moharamfatema@users.noreply.github.com>
2023-01-06 14:39:02 +0200

    Create dominoPiling.cpp

---
+++ codeForces/dominoPiling.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int area,temp;
+    cin >> area;
+    cin >> temp;
+    area *= temp;
+
+    cout << area / 2 << "\n";
+
+    return 0;
+}
